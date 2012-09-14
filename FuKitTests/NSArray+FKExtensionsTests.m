5747fba5ae6241cea6da21d4557b9646427429ac
Pyry Jahkola <pyry.jahkola@futurice.com>
2012-09-14 17:54:08 +0300

    Add unit test for fk_filter:

--- FuKitTests/NSArray+FKExtensionsTests.m
+++ FuKitTests/NSArray+FKExtensionsTests.m
@@ -37,4 +37,24 @@
     STAssertEqualObjects([array1 fk_map:describe], array3, @"map function");
 }
 
+- (void)test_fk_filter
+{
+    FKArrayMatchBlock valid = ^BOOL(id obj) { return obj != [NSNull null]; };
+
+    NSArray *array0 = @[];
+    NSArray *array1 = @[@1, [NSNull null], @"three"];
+    NSArray *array2 = @[@1, @2.0, @"three", @[], @{@123: [NSNull null]}];
+    NSArray *array3 = @[[NSNull null], @"2", @"three", [NSNull null]];
+
+    if (![array0 respondsToSelector:@selector(fk_filter:)]) {
+        STFail(@"respond to fk_filter:");
+        return;
+    }
+
+    STAssertEqualObjects([array0 fk_filter:valid], @[], @"filter empty array");
+    STAssertEqualObjects([array1 fk_filter:valid], (@[@1, @"three"]), @"filter array1");
+    STAssertEqualObjects([array2 fk_filter:valid], array2, @"filter array2");
+    STAssertEqualObjects([array3 fk_filter:valid], (@[@"2", @"three"]), @"filter array3");
+}
+
 @end
